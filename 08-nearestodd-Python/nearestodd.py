c25ebfe95eff58763e9afd83d5a43d8acb913300
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-08-10 10:53:23 +0530

    final commit

--- 08-nearestodd-Python/nearestodd.py
+++ 08-nearestodd-Python/nearestodd.py
@@ -2,10 +2,16 @@
 # and returns as an int value the nearest odd number to n. 
 # In the case of a tie, return the smaller odd value. 
 # Note that the result must be an int, so nearestOdd(13.0) is the int 13, and not the float 13.0.
-
+import math
 
 
 def fun_nearestodd(n):
-	return 0
+	n = math.ceil(n)    #get the ceil value of the given num
+	if(n%2 == 0):       #check if it is even, if yes, subtract 1 
+		return((n-1))   #as we need the smaller odd, 
+	else:
+		return(int(n))  #else return the num
+
+
 
 
