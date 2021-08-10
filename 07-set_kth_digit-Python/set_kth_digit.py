fea6db881897fa032f4fb76804122c3adf3f9491
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-08-10 10:40:51 +0530

    final commit

--- 07-set_kth_digit-Python/set_kth_digit.py
+++ 07-set_kth_digit-Python/set_kth_digit.py
@@ -7,5 +7,18 @@
 
 
 def fun_set_kth_digit(n, k, d):
-		return 0
-
+    num=abs(n)                          #convert the num into positive
+    n_list=[int(i) for i in str(num)]   #converting num into list of nums
+    if(k==len(n_list)):                 #case-1: if k==len(num_list), just append the d at 
+        n_list.insert(0,d)              #the beginning of list
+        ans=n_list
+    else:                               #case-2: if(len(k)<k), 
+        n_list=n_list[::-1]             #reverse the list, a/c to the condition counting starts from right side
+        n_list[k]=d                     #replace the kth index with digit "d"
+        ans=n_list[::-1]                #and then reverse it to get the initial order
+    
+    answer=[str(i) for i in ans]        #as we have the ans in the form of list
+    answer=int("".join(answer))         #convert it into int
+    if(n<0):                            #case-3: if the num 'n' is negative, initially -> return -ve num
+        return (-answer)                #else return +ve num
+    return (answer)
