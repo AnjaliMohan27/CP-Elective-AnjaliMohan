da9c20ba2ec5a616384ec80d88d32497a193bee3
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-08-13 13:01:14 +0530

    first commit

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -2,6 +2,41 @@
 # Write the function mostFrequentDigit(n), that takes a non-negative integer n and returns the digit from 0 to 9 
 # that occurs most frequently in it, with ties going to the smaller digit.
 
+def countOccurrences(x, d):
+    count = 0 # Initialize count
+            # of digit d
+    while (x):
+        
+        # Increment count if current
+        # digit is same as d
+        if (x % 10 == d):
+            count += 1
+        x = int(x / 10)
+
+    return count
+
+
+
 def mostfrequentdigit(n):
-	# your code goes here
-	pass
+    # Handle negative number
+    if (n < 0):
+        n = -n
+    
+    result = 0 # Initialize result
+                # which is a digit
+    max_count = 1 # Initialize count
+                # of result
+    
+    # Traverse through all digits
+    for d in range(10):
+        
+        # Count occurrences of current digit
+        count = countOccurrences(n, d);
+        
+        # Update max_count and
+        # result if needed
+        if (count >= max_count):
+            max_count = count
+            result = d
+        
+    return result
