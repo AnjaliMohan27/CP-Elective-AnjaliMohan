4f96993a8d3020415f867ee8dde985a00ddd13ae
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-08-16 13:58:13 +0530

    final commit

--- 10-mostfrequencydigit-Python/mostfrequentdigit.py
+++ 10-mostfrequencydigit-Python/mostfrequentdigit.py
@@ -2,41 +2,23 @@
 # Write the function mostFrequentDigit(n), that takes a non-negative integer n and returns the digit from 0 to 9 
 # that occurs most frequently in it, with ties going to the smaller digit.
 
-def countOccurrences(x, d):
-    count = 0 # Initialize count
-            # of digit d
-    while (x):
-        
-        # Increment count if current
-        # digit is same as d
-        if (x % 10 == d):
-            count += 1
-        x = int(x / 10)
-
-    return count
-
-
 
 def mostfrequentdigit(n):
-    # Handle negative number
-    if (n < 0):
-        n = -n
-    
-    result = 0 # Initialize result
-                # which is a digit
-    max_count = 1 # Initialize count
-                # of result
-    
-    # Traverse through all digits
-    for d in range(10):
-        
-        # Count occurrences of current digit
-        count = countOccurrences(n, d);
-        
-        # Update max_count and
-        # result if needed
-        if (count >= max_count):
-            max_count = count
-            result = d
-        
+    num_list=[int(i) for i in str(n)]
+    n=len(num_list)
+    num_list.sort()
+    max_count=1
+    result=num_list[0]
+    current_count=1
+    for i in range(1,n):
+        if(num_list[i]==num_list[i-1]):
+            current_count=current_count+1
+        else:
+            if(current_count>max_count):
+                max_count=current_count
+                result=num_list[i-1]
+            current_count=1
+    if(current_count>max_count):
+        max_count=current_count
+        result=num_list[n-1]
     return result
