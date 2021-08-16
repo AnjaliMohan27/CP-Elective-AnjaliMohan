a98ea401567faa05d3bc235093926a4696698073
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-08-16 12:46:56 +0530

    final

--- 03-applycaesarcipher-Python/applycaesarcipher.py
+++ 03-applycaesarcipher-Python/applycaesarcipher.py
@@ -12,7 +12,7 @@
 
 def fun_applycaesarcipher(msg, shift):
     resultmsg = []
-    newindex = []
+  
     
     uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -21,13 +21,11 @@ def fun_applycaesarcipher(msg, shift):
         if eachLetter in uppercase:
             index = uppercase.index(eachLetter)
             crypting = (index + shift) % 26
-            newindex.append(crypting)
             newLetter = uppercase[crypting]
             resultmsg.append(newLetter)
         elif eachLetter in lowercase:
             index = lowercase.index(eachLetter)
             crypting = (index + shift) % 26
-            newindex.append(crypting)
             newLetter = lowercase[crypting]
             resultmsg.append(newLetter)
         elif eachLetter==" ":
