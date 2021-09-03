2c5516e704ffdb950fcd876e01a4a152f01fb4ba
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-09-03 09:28:00 +0530

    final commit

--- 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
+++ 04-nth_automorphicnumber-Python/nthautomorphicnumber.py
@@ -4,5 +4,23 @@
 # 76 and 890625 are all automorphic numbers.
 
 def nthautomorphicnumbers(n):
-	# Your code goes here
-	pass
+    c = 0
+    letest = 0
+    num = 1
+    while c != n:
+        num1 = num
+        sqr = num1 * num1
+        flag = 0
+        while num1>0:
+            if num1%10 != sqr%10:
+                flag = -1
+                break
+            num1 = num1 // 10
+            sqr = sqr // 10
+
+        if flag==0:
+            c+=1
+            letest = num
+
+        num = num + 1
+    return letest
