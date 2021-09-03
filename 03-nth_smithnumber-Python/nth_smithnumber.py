072570be74301508a0df990f473dd0ad9b94ee9b
ANJALI MOHAN UPPU <anju4future@msitprogram.net>
2021-09-03 13:08:05 +0530

    final commit

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -6,7 +6,48 @@
 # counted twice, thus making 4 a Smith Number.
 # so fun_nthsmithnumber(0) should return 4
 # so fun_nthsmithnumber(1) should return 22
+def isPrime(n): # from course notes
+    if (n < 2):
+        return False
+    if (n == 2):
+        return True
+    if (n % 2 == 0):
+        return False
+    maxFactor = int(round(n**0.5))
+    for factor in range(2,maxFactor+1):
+        if (n % factor == 0):
+            return False
+    return True
+
+def sumOfDigit(n):
+    acc = 0
+    while(n/10!=0):
+        acc += n%10
+        n = n/10
+    acc += n
+    return acc
+
+def isSmithNumber(n):
+    if(isPrime(n)): return False
+    sumOfFactor = 0
+    sumOfNumber = sumOfDigit(n)
+    while not(isPrime(n)):
+        maxFactor = int(round(n**0.5))
+        for factor in range(2, maxFactor+1):
+            if(n%factor == 0)and(isPrime(factor)):  #add sum of the digit of factor every loop
+                sumOfFactor += sumOfDigit(factor)
+                n = n/factor
+                break
+    sumOfFactor += sumOfDigit(n)
+    return (sumOfFactor == sumOfNumber)
 
 
 def fun_nth_smithnumber(n):
-    return 1
+    if(n==0): return 4         #the first one is 4
+    i = -1
+    x = 4
+    while (i<n):
+        if(isSmithNumber(x)):   #find the nth smith number
+            i += 1
+        x += 1
+    return x-1
